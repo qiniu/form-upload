94ec7b187f71d88fcfe51ae754c9c094baa92d14
xushiwei <xushiweizh@gmail.com>
2013-05-28 04:23:09 +0800

    mv PutPolicy to github.com/qiniu/api

--- web/golang/main.go
+++ web/golang/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	"os"
 	"log"
 	"fmt"
-	"os"
-	"time"
 	"net/http"
 	"encoding/base64"
 	"encoding/json"
 	"github.com/qiniu/api/rs"
-	"github.com/qiniu/api/auth/digest"
 	. "github.com/qiniu/api/conf"
 )
 
@@ -17,27 +15,6 @@ const (
 	BUCKET = "a"
 	DOMAIN = "aatest.qiniudn.com"
 )
-
-// --------------------------------------------------------------------------------
-
-type PutPolicy struct {
-	Scope            string `json:"scope,omitempty"`
-	CallbackUrl      string `json:"callbackUrl,omitempty"`
-	CallbackBody     string `json:"callbackBody,omitempty"`
-	ReturnUrl        string `json:"returnUrl,omitempty"`
-	ReturnBody       string `json:"returnBody,omitempty"`
-	AsyncOps         string `json:"asyncOps,omitempty"`
-	EndUser          string `json:"endUser,omitempty"`
-	Expires          uint32 `json:"deadline"` 			// 截止时间（以秒为单位）
-}
-
-func (r PutPolicy) Token() string {
-	if r.Expires == 0 {
-		r.Expires = 3600
-	}
-	r.Expires += uint32(time.Now().Unix())
-	return digest.SignJson(ACCESS_KEY, []byte(SECRET_KEY), &r)
-}
 
 // --------------------------------------------------------------------------------
 
@@ -94,15 +71,14 @@ func handleReturn(w http.ResponseWriter, req *http.Request) {
 	}
 
 	policy := rs.GetPolicy{Scope: "*/" + uploadRet.Key}
-	token := policy.Token()
-	img := "http://" + DOMAIN + "/" + uploadRet.Key + "?token=" + token
+	img := policy.MakeRequest("http://" + DOMAIN + "/" + uploadRet.Key)
 	returnPage := fmt.Sprintf(returnPageFmt, string(b), img)
 	w.Write([]byte(returnPage))
 }
 
 func handleUpload(w http.ResponseWriter, req *http.Request) {
 
-	policy := PutPolicy{Scope:BUCKET, ReturnUrl: "http://localhost:8765/uploaded"}
+	policy := rs.PutPolicy{Scope: BUCKET, ReturnUrl: "http://localhost:8765/uploaded"}
 	token := policy.Token()
 	log.Println("token:", token)
 	uploadForm := fmt.Sprintf(uploadFormFmt, token)
